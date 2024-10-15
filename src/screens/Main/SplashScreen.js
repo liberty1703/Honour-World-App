e4a3dee6e7d0ff68336956ee0bb65d4522b1eac8
liberty1703 <libertyabraham46@gmail.com>
2024-10-15 14:24:16 +0100

    Updated timer

--- src/screens/Main/SplashScreen.js
+++ src/screens/Main/SplashScreen.js
@@ -1,15 +1,24 @@
 import { StatusBar, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { COLORS, FONTS } from '../../constants'
+import { useNavigation } from '@react-navigation/native'
 
 const SplashScreen = () => {
+    const navigation = useNavigation();
+    useEffect(()=>{
+        const timer = setTimeout(()=>{
+            navigation.replace('Login')
+        },3000)
+    },[])
+
+
   return (
     <View style={styles.page}>
     <StatusBar
     backgroundColor={COLORS.primary}
     barStyle={'light-content'}
     />
-      <Text style={{...FONTS.h1, }}>HonourWorld</Text>
+      <Text style={{...FONTS.h1, textAlign: 'center', color: COLORS.white }}>HonourWorld</Text>
     </View>
   )
 }
