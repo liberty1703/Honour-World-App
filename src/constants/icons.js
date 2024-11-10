d5e6b51647e56d66350cad28156b3b273a989d7f
Ayanyiola Enoch <ayanyiolaenoch155@gmail.com>
2024-11-10 17:38:35 +0100

    Added new service tab

--- src/constants/icons.js
+++ src/constants/icons.js
@@ -8,40 +8,41 @@ const lock = require('../assets/icons/lock.png');
 const name = require('../assets/icons/name.png');
 const phone = require('../assets/icons/phone.png');
 const whatsapp = require('../assets/icons/whatsapp.png');
-const home = require('../assets/icons/home.png')
-const notification = require('../assets/icons/notification.png')
-const copy = require('../assets/icons/copy.png')
-const cash = require('../assets/icons/cash.png')
-const transfer = require('../assets/icons/transfer.png')
-const more = require('../assets/icons/more.png')
-const phonecall = require('../assets/icons/phonecall.png')
-const data = require('../assets/icons/data.png')
-const cabletv = require('../assets/icons/cabletv.png')
-const mtn = require('../assets/icons/mtn.png')
-const ninemobile = require('../assets/icons/ninemobile.png')
-const airtel = require('../assets/icons/airtel.png')
-const glo = require('../assets/icons/glo.png')
-const contacts = require('../assets/icons/contacts.png')
-const caution = require('../assets/icons/caution.png')
-const validation = require('../assets/icons/validation.png')
-const check = require('../assets/icons/check.png')
-const share = require('../assets/icons/share.png')
-const service = require('../assets/icons/service.png')
-const electricity = require('../assets/icons/electricity.png')
-const education = require('../assets/icons/education.png')
-const airtime = require('../assets/icons/airtime.png')
-const autobuy = require('../assets/icons/autobuy.png')
-const cgwallet = require('../assets/icons/cgwallet.png')
-const biz = require('../assets/icons/biz.png')
-const wallet = require('../assets/icons/wallet.png')
-const user = require('../assets/icons/user.png')
-const arrowright = require('../assets/icons/arrowright.png')
-const wema = require('../assets/icons/wema.png')
-const circlered = require('../assets/icons/circlered.png')
-const boxarrrow = require('../assets/icons/boxarrrow.png')
-const monniepoint = require('../assets/icons/monniepoint.png')
-const card = require('../assets/icons/card.png')
-const droparrow = require('../assets/icons/droparrow.png')
+const home = require('../assets/icons/home.png');
+const notification = require('../assets/icons/notification.png');
+const copy = require('../assets/icons/copy.png');
+const cash = require('../assets/icons/cash.png');
+const transfer = require('../assets/icons/transfer.png');
+const more = require('../assets/icons/more.png');
+const phonecall = require('../assets/icons/phonecall.png');
+const data = require('../assets/icons/data.png');
+const cabletv = require('../assets/icons/cabletv.png');
+const mtn = require('../assets/icons/mtn.png');
+const ninemobile = require('../assets/icons/ninemobile.png');
+const airtel = require('../assets/icons/airtel.png');
+const glo = require('../assets/icons/glo.png');
+const contacts = require('../assets/icons/contacts.png');
+const caution = require('../assets/icons/caution.png');
+const validation = require('../assets/icons/validation.png');
+const check = require('../assets/icons/check.png');
+const share = require('../assets/icons/share.png');
+const service = require('../assets/icons/service.png');
+const service2 = require('../assets/icons/service2.png');
+const electricity = require('../assets/icons/electricity.png');
+const education = require('../assets/icons/education.png');
+const airtime = require('../assets/icons/airtime.png');
+const autobuy = require('../assets/icons/autobuy.png');
+const cgwallet = require('../assets/icons/cgwallet.png');
+const biz = require('../assets/icons/biz.png');
+const wallet = require('../assets/icons/wallet.png');
+const user = require('../assets/icons/user.png');
+const arrowright = require('../assets/icons/arrowright.png');
+const wema = require('../assets/icons/wema.png');
+const circlered = require('../assets/icons/circlered.png');
+const boxarrrow = require('../assets/icons/boxarrrow.png');
+const monniepoint = require('../assets/icons/monniepoint.png');
+const card = require('../assets/icons/card.png');
+const droparrow = require('../assets/icons/droparrow.png');
 export default {
     arrow,
     box,
@@ -71,6 +72,7 @@ export default {
     validation,
     check,
     share,
+    service2,
     service,
     electricity,
     education,
