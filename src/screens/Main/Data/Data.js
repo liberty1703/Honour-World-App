847368b33555c939c49bed6ad2eda5c51086fa17
Ayanyiola Enoch <ayanyiolaenoch155@gmail.com>
2024-11-10 17:34:26 +0100

    lets begin

--- src/screens/Main/Data/Data.js
+++ src/screens/Main/Data/Data.js
@@ -2,13 +2,11 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { useNavigation } from '@react-navigation/native'
 import { SIZES, COLORS, icons, images } from '../../../constants'
-import ArrowButton2 from '../../../components/Button/ArrowButon2'
 import ArrowButton1 from '../../../components/Button/ArrowButton1'
-
 const Data = () => {
     const navigation = useNavigation();
     return (
-        <View styles={styles.page}>
+        <View styles={{ paddingHorizontal: SIZES.h3, paddingTop: SIZES.h3 }}>
             <ArrowButton1 text={'Data'} onPress={() => navigation.goBack()} />
             <View>
                 <Text style={{ fontSize: SIZES.h4 }}>This</Text>
