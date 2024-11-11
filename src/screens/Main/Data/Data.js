c5bc59dd1282b0a72d708070260ab2827c1e62e6
Ayanyiola Enoch <ayanyiolaenoch155@gmail.com>
2024-11-12 00:09:27 +0100

    added phone number field in data screen...

--- src/screens/Main/Data/Data.js
+++ src/screens/Main/Data/Data.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View, TextInput, Image } from 'react-native'
 import React from 'react'
 import { useNavigation } from '@react-navigation/native'
 import { SIZES, COLORS, FONTS, icons, images } from '../../../constants'
@@ -12,6 +12,20 @@ const Data = () => {
                 <Text style={{ ...FONTS.h4, marginTop: SIZES.h1 }}>
                     Selected Network
                 </Text>
+
+                <View style={styles.textinputstyl}>
+                    <TextInput
+                        placeholder='Enter Phone Number'
+                        style={{ ...FONTS.h4, flex: 1, backgroundColor: COLORS.grey }}
+                    />
+                    <TouchableOpacity style={{ alignSelf: 'center' }}>
+                        <Image
+                            source={icons.contacts}
+                            style={{ height: SIZES.h3 * 2, width: SIZES.h3 * 2 }}
+                        />
+                    </TouchableOpacity>
+                </View>
+
             </View>
         </View>
     )
@@ -25,6 +39,15 @@ const styles = StyleSheet.create({
         // flex: 1,
         paddingHorizontal: SIZES.h3,
         paddingTop: SIZES.h1,
+    },
+    textinputstyl: {
+        height: SIZES.h1 * 2,
+        backgroundColor: COLORS.grey,
+        borderRadius: 5,
+        borderColor: COLORS.black,
+        marginTop: SIZES.h3,
+        justifyContent: 'center',
+        flexDirection: 'row'
     },
 
 });
